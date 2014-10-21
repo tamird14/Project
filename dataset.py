e7c9e420ef6d5a0f6817cce4d4bfb35081d50dd5
tamir <tamird14@gmail.com>
2014-10-22 00:07:26 +0300

    first - 14/10/14

--- dataset.py
+++ dataset.py
@@ -1,7 +1,6 @@
 __author__ = 'Tamir'
 
-import bson
-import json
+from random import randint
 import pymongo
 import re
 
@@ -11,26 +10,33 @@ IN_DB = "<--in_db-->"
 CHANGED_CODE = re.compile('<--[0-9]+-->')
 NOT_IN_DB = "<--not_in_db-->"
 
-txt_file = open(TEST_COLLECTION, 'r')
-i = 0
-for query in txt_file.read().split("<--NEW CODE-->"):
-    if len(query) > 0:
-        if IN_DB==query.split('\n')[1]:
-            print "in DB"
-        elif CHANGED_CODE.match(query.split('\n')[1]):
-            print "changed"
-        elif NOT_IN_DB==query.split('\n')[1]:
-            print "not in DB"
-        #else:
-
-
-#if __name__ == '__main__':
+# if __name__ == '__main__':
 client = pymongo.MongoClient()
 db = client.dataset
 documents = db.all_queries
 queries = db.test_queries
-i=0
-for doc in documents.find():
-    if i==1:
-        print doc
-    i+=1
+
+txt_file = open(TEST_COLLECTION, 'r')
+used = []
+count = 0
+for count in range(0, 25):
+    codeID = randint(0, 999)
+    while codeID in used:
+        codeID = randint(0, 999)
+    used.append(codeID)
+    #queries.insert({"code_id": count, "code": documents.find({'code_id': str(codeID)})[0]['code'], "type": "in_db"})
+    count += 1
+
+for query in txt_file.read().split("<--NEW CODE-->"):
+    if len(query) > 0:
+        if CHANGED_CODE.match(query.split('\n')[1]):
+            break
+            #queries.insert({"code_id": count, "code": query.split(query.split('\n')[1])[1], "type": "changed"})
+        elif NOT_IN_DB == query.split('\n')[1]:
+            break
+            #queries.insert({"code_id": count, "code": query.split(query.split('\n')[1])[1], "type": "not_in_db"})
+        else:
+            break
+        count += 1
+
+print queries.find()[25]['code']
